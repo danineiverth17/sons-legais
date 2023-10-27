56bcba327bc8a21cddb60969fcc449b3efc6265a
danineiverth17 <105320005+danineiverth17@users.noreply.github.com>
2023-10-27 11:24:34 -0300

    ok

--- css/main.js
+++ css/main.js
@@ -1,6 +1,16 @@
-//Declaração/criação da função tocaSomPom
-function tocaSomPom(){
-    document.querySelector(`#sounds/acessa-ai-estourado-mt-estourado.mp3`).play();
-    }
-    //chamada ou invocação da função tocaSomPom ao clicar no botão
-    document.querySelector(`#sounds/acessa-ai-estourado-mt-estourado.mp3`).onclick = tocaSomPom;
+//Declação/criação da função
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+//criando a referências constante listaDeTeclas e buscando a classe
+const listaDeTeclas = document.querySelectorAll('.tecla');
+let contador = 0;
+while(contador < listaDeTeclas.length){
+    const efeito = listaDeTeclas [contador].classlist[1];
+    const idAudio = "#som_" + efeito;
+    listaDeTeclas[contador].onclick = function(){
+        tocaSom(idAudio);
+    } 
+    contador = contador + 1;
+    console.log(contador);
+}
