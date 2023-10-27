eae25005bf531af2664b341b884f1210861be046
danineiverth17 <105320005+danineiverth17@users.noreply.github.com>
2023-10-27 10:59:59 -0300

    .

---
+++ css/main.js
@@ -0,0 +1,6 @@
+//Declaração/criação da função tocaSomPom
+function tocaSomPom(){
+    document.querySelector(`#sounds/acessa-ai-estourado-mt-estourado.mp3`).play();
+    }
+    //chamada ou invocação da função tocaSomPom ao clicar no botão
+    document.querySelector(`#sounds/acessa-ai-estourado-mt-estourado.mp3`).onclick = tocaSomPom;
